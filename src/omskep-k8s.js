9c5a611b114018a3d2e088a44de59f442e9442ce
bs595r <bs595r@att.com>
2020-04-02 18:27:22 -0700

    Updates getDeployInfo method to include more data

--- src/omskep-k8s.js
+++ src/omskep-k8s.js
@@ -7,11 +7,23 @@ let k8s = {
     * Gets all the deployments names json file
     */
     getDeployInfo() {
-        if (this.defn.items) {
-            return this.defn.items.map(x => ({name: (x.metadata.name || ''), replicas: (x.spec.replicas)}));
-            // return this.defn.items;
+        //
+        // If there are multiple deployments per file
+        // then there will be an items array
+        //
+        let ret = [];
+        let items = this.defn.items ? this.defn.items : [this.defn];
+        for (const i of items) {
+            let name = i.metadata.name || '';
+            let replicas = i.spec.replicas || 0;;
+            let labels = i.metadata.labels || '';
+            // taking first container and first port, not sure if this will be a problem or not
+            let portnum = i.spec.template.spec.containers[0].ports[0].containerPort || '';
+            ret.push({name: name, replicas: replicas, labels: labels, portnum: portnum});
         }
-        return this.defn.metadata || '';
+            // console.log(this.defn.items[0].spec.template.spec.containers[0].ports[0].containerPort);
+            // return this.defn.items.map(x => ({name: (x.metadata.name || ''), replicas: (x.spec.replicas), labels: (x.metadata.labels)}));
+        return ret;
     },
     /**
     * Gets the namespace for the deployment
