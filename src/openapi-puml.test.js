edfb0ba0ca45a6cb3b4659955420d4594f8a1d5f
Brett Schwarz <brett.schwarz@gmail.com>
2020-11-18 17:23:13 -0800

    chore: adds tests for theme

--- src/openapi-puml.test.js
+++ src/openapi-puml.test.js
@@ -11,3 +11,13 @@ beforeEach(() => {
 test('Get Title of the Openapi Definition', () => {
     expect(diag.getTitle()).toBe('Swagger Petstore');
 });
+test('Set and Get Puml Theme', () => {
+    diag.theme = 'sandstone';
+    let str = '!include https://raw.githubusercontent.com/bschwarz/puml-themes/master/themes/sandstone/puml-theme-sandstone.puml';
+    expect(diag.theme).toBe(str);
+});
+test('Set and Get Non Puml Theme', () => {
+    diag.theme = 'sandstoneX';
+    let str = '!include sandstoneX';
+    expect(diag.theme).toBe(str);
+});
