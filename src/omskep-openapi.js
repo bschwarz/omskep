3d9c30c239544071ba0ccd11364acc2e406c1b7d
Brett Schwarz <brett.schwarz@gmail.com>
2022-04-07 11:36:07 -0700

    updates from laag

--- src/omskep-openapi.js
+++ src/omskep-openapi.js
@@ -24,7 +24,12 @@ let openapi3 = {
         let methods = new Set();
 
         for (let P of Object.keys(this.defn.paths)) {
-            Object.keys(this.defn.paths[P]).forEach(item => methods.add(item));
+            // Object.keys(this.defn.paths[P]).forEach(item => methods.add(item));
+            for (let M of Object.keys(this.defn.paths[P])) {
+                if (this.httpMethods.includes(M)) {
+                    methods.add(M);
+                }
+            }
         }
         return Array.from(methods);
     },
@@ -156,7 +161,7 @@ let openapi3 = {
     * @returns (String) - returns object containing the path information
     */
      getPath(path) {
-        return this.dictKeysExists(this.doc, 'paths', path) ? this.doc.paths[path] : {};
+        return this.dictKeysExists(this.defn, 'paths', path) ? this.defn.paths[path] : {};
     },
     /**
     * Helper method to see if keys exist in an object
@@ -173,6 +178,122 @@ let openapi3 = {
         }
 
         return true;
+    },
+    /**
+    * Retrieves the components schemas object
+    * @returns (Object) - returns object containing all of the components information
+    */
+    getComponentsSchemas() {
+        return this.dictKeysExists(this.defn, 'components', 'schemas') ? this.defn.components.schemas : {};
+    },
+    /**
+    * gets the response object for a path/verb
+    * @param {string} path - the path segment of the resource
+    * @param {string} verb - HTTP verb of the operation
+    * @param {string} statusCode - HTTP status code of the operation
+    */
+    getOperationResponse(path, verb, statusCode) {
+        let ret = {};
+        if (! this.operationExists(path, verb)) return {};
+        
+        if (this.dictKeysExists(this.defn.paths[path][verb],'responses', statusCode)) {
+            ret = this.defn.paths[path][verb]['responses'][statusCode];
+        }
+
+        return ret;
+    },
+    /**
+    * gets the request object for a path/verb
+    * @param {string} path - the path segment of the resource
+    * @param {string} verb - HTTP verb of the operation
+    */
+    getOperationRequest(path, verb) {
+        let ret = {};
+        if (! this.operationExists(path, verb)) return {};
+        
+        if (this.dictKeysExists(this.defn.paths[path][verb],'requestBody', 'content')) {
+            ret = this.defn.paths[path][verb]['requestBody'];
+        }
+
+        return ret;
+    },
+    /**
+    * gets the media types for an operation response
+    * @param {string} path - the path segment of the resource
+    * @param {string} verb - HTTP verb of the operation
+    * @param {integer} code - HTTP status code of the operation
+    */
+     getOperationResponseMedia(path, verb, code=null) {
+         let ret = [];
+         verb = verb.toLocaleLowerCase();
+         if (! this.operationExists(path, verb)) return [];
+         
+         if (! code) {
+             code = this.getSuccessCode(path, verb);
+         }
+         if (this.dictKeysExists(this.defn.paths[path][verb],'responses', code, 'content')) {
+             ret = Object.keys(this.defn.paths[path][verb]['responses'][code]['content']);
+         }
+ 
+         return ret;
+    },
+    /**
+    * gets the parameters for an operation
+    * @param {string} path - the path of the resource
+    * @param {string} verb - HTTP verb of the operation. If 'path' is specified, then it just gets the parameters that are for all methods of a path
+    * @param {boolean} all - if true, will return the parameters for verb as well as any path relative parameters
+    */
+    getOperationParameters(path, verb, all=false) {
+        verb = verb.toLowerCase();
+        let ret = [];
+        let pathobj = this.getPath(path);
+        let obj;
+        if (verb === 'path') {
+            obj = pathobj;
+        } else {
+            if (! pathobj[verb]) {
+                return [];
+            }
+            obj = pathobj[verb];
+        }
+        if (obj.parameters) {
+            for (let item of obj.parameters) {
+                let obj = {};
+                obj.resource = path;
+                obj.method = verb;
+                obj.name = item.name;
+                obj.location = item.in;
+                obj.required = item.required || false;
+                obj.type = item.schema.type;
+                obj.description = item.description;
+                ret.push(obj);
+            }
+        }
+        if (all) {
+            let params = pathobj.parameters || [];
+            for (let item of params) {
+                let obj = {};
+                obj.resource = path;
+                obj.method = verb;
+                obj.name = item.name;
+                obj.location = item.in;
+                obj.required = item.required || false;
+                obj.type = item.schema.type;
+                obj.description = item.description;
+                ret.push(obj);
+            }
+        }
+        return ret;
+    },
+    /**
+    * gets a boolean on whether the operation is deprecated or not
+    * @param {string} path - the path of the resource
+    * @param {string} verb - HTTP verb of the operation
+    */
+     isOperationDeprecated(path, verb) {
+        verb = verb.toLowerCase();
+        if (! this.operationExists(path, verb)) return true;
+        return this.defn.paths[path][verb]['deprecated'] || false;
     }
 }
 
